b4f42e516b78c21c5c6b8c52b97547828a14c011
Alexis60 <alexis-lecompte@hotmail.fr>
2016-05-10 11:55:22 +0200

    Exercice 5 finalisé

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+$firstvariable = "Premiére chaîne de caractéres";
+$secondvariable = "Seconde chaîne de caractéres";
+function blblb($firstvariable, $secondvariable){
+  return $firstvariable.$secondvariable;
+}
+$retour = blblb($firstvariable, $secondvariable);
+echo $retour;
+?>
